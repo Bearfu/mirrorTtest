ee6f02c1b4fba4fceedc63a6c285bcd3e42750d7
傅 喆 <303761829@qq.com>
2019-08-21 18:59:39 +0800

    首页 转发方案

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
-FROM ccr.ccs.tencentyun.com/dhub.wscn.com/alpine:3.5
-ENV CONFIGOR_ENV ivktest
-ADD server /
-ADD conf/ /conf
-ENTRYPOINT [ "/server" ]
+# 创建镜像，注意修改配置
+$ docker build -t hbchen/echo-web:v0.0.1 .
+
+# 运行容器
+$ docker run
+     -p 8081:8081
+     --name=echo-web
+     hbchen/echo-web:v0.0.1
--- main.go
+++ main.go
@@ -1,24 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo"
+	"net/http"
 )
 
 func main() {
-	e := echo.New()
-	e.Static("/", "public")
-	e.GET("/", func(c echo.Context) error {
-		c.Response().Writer.Header().Set("Accept-Ranges", "bytes")
-		c.Response().Writer.Header().Set("ETag", "5d5bd2b1-2402")
-		c.Response().Writer.Header().Set("Connection", "keep-alive")
-		c.Response().Writer.Header().Set("Last-Modified", "Tue, 20 Aug 2019 11:30:01 GMT")
-		c.Response().Writer.Header().Set("Content-Length", "9218")
-		c.Response().Writer.Header().Set("Content-Type", "text/html; charset=utf-8")
-		c.Response().Writer.Header().Set("Date", "Tue, 20 Aug 2019 11:32:36 GMT")
-		c.Response().Writer.Header().Set("Server", "nginx/1.10.2")
-		return c.File("public/index.html")
-		//return c.String(http.StatusOK, "Hello, World!")
+	//e := echo.New()
+	//e.Static("/", "public")
+	//e.GET("/", func(c echo.Context) error {
+	//	c.Response().Writer.Header().Set("Accept-Ranges", "bytes")
+	//	c.Response().Writer.Header().Set("ETag", "5d5bd2b1-2402")
+	//	c.Response().Writer.Header().Set("Connection", "keep-alive")
+	//	c.Response().Writer.Header().Set("Last-Modified", "Tue, 20 Aug 2019 11:30:01 GMT")
+	//	c.Response().Writer.Header().Set("Content-Length", "9218")
+	//	c.Response().Writer.Header().Set("Content-Type", "text/html; charset=utf-8")
+	//	c.Response().Writer.Header().Set("Date", "Tue, 20 Aug 2019 11:32:36 GMT")
+	//	c.Response().Writer.Header().Set("Server", "nginx/1.10.2")
+	//	return c.File("public/index.html")
+	//})
+	//e.Logger.Fatal(e.Start(":8081"))
+
+	ec := echo.New()
+	ec.Static("/", "public")
+	ec.GET("/", func(c echo.Context) error {
+		return c.Redirect(http.StatusMovedPermanently, "http://mirror.azure.cn/")
 	})
-	e.Logger.Fatal(e.Start(":8081"))
+	ec.GET("/:parmar", func(c echo.Context) error {
+		Param := c.Param("parmar")
+		fmt.Println(Param)
+		return c.Redirect(http.StatusMovedPermanently, "http://mirror.azure.cn/"+Param)
+	})
+	ec.Logger.Fatal(ec.Start(":8082"))
 
 }
