d9cdb604416efdf71bd4e008eefe15cd14d89fc2
傅 喆 <303761829@qq.com>
2019-08-25 23:50:07 +0800

    首页

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"github.com/labstack/echo"
-	"net/http"
 )
 
 func main() {
@@ -21,17 +19,4 @@ func main() {
 		return c.File("public/index.html")
 	})
 	e.Logger.Fatal(e.Start(":8081"))
-
-	ec := echo.New()
-	ec.Static("/", "public")
-	ec.GET("/", func(c echo.Context) error {
-		return c.Redirect(http.StatusMovedPermanently, "http://mirror.azure.cn/")
-	})
-	ec.GET("/:parmar", func(c echo.Context) error {
-		Param := c.Param("parmar")
-		fmt.Println(Param)
-		return c.Redirect(http.StatusMovedPermanently, "http://mirror.azure.cn/"+Param)
-	})
-	ec.Logger.Fatal(ec.Start(":8082"))
-
 }
